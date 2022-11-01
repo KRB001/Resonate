5abbaa802cd18a22e4158a466c24ef6b23e03047
KRB001 <kieranbentley0@gmail.com>
2022-11-01 10:25:58 -0400

    Began work on routes, added models.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
+from flask import render_template, redirect, url_for, flash, request
 from app import app
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
+from app.models import *
+import datetime
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "RESONATE IN-PROGRESS"
+    return "INDEX"
+
+
+@app.route('/discover')
+def discover():
+    return "DISCOVER"
+
