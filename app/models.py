481583c7ae399b004429ae351db0aef2b6d910b5
CRaineault <cmraineault@gmail.com>
2022-11-04 10:42:43 -0400

    Attempted to fix issues with database relationships between the same class

--- app/models.py
+++ app/models.py
@@ -1,7 +1,4 @@
-from sqlalchemy import cast
-from sqlalchemy.orm import relationship
-
-from app import db, login # TO DO SOON: add db and login setup stuff to __init.py__
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
@@ -17,18 +14,8 @@ class User(UserMixin, db.Model):
     following_public = db.Column(db.Boolean)
     # other settings for any account type go here
 
-    followers = relationship(
-        "Follows",
-        primaryjoin=id == id,
-        foreign_keys=id,
-        remote_side=id,
-    )
-    followed_accounts = relationship(
-        "Follows",
-        primaryjoin=id == id,
-        foreign_keys=id,
-        remote_side=id,
-    )
+    followers = db.relationship('Follows', backref='followed_account', lazy='dynamic')
+    followed_accounts = db.relationship('Follows', backref='follower', lazy='dynamic')
     frequent_artists = db.relationship('ArtistToListener', backref='listener', lazy='dynamic')
     frequent_genres = db.relationship('ListenerToGenre', backref='listener', lazy='dynamic')
 
@@ -60,19 +47,8 @@ class Artist(User, db.Model):
     location = db.Column(db.String(200), index=True)
     # other artist only settings/info goes here
 
-    similar = relationship(
-        "SimilarArtist",
-        primaryjoin=id == id,
-        foreign_keys=id,
-        remote_side=id,
-    )
-    referenced_similar = relationship(
-        "SimilarArtist",
-        primaryjoin=id == id,
-        foreign_keys=id,
-        remote_side=id,
-    )
-
+    similar = db.relationship('SimilarArtist', backref='artist_account', lazy='dynamic')
+    referenced_similar = db.relationship('SimilarArtist', backref='artist_referenced', lazy='dynamic')
     genres = db.relationship('ArtistGenre', backref='artist', lazy='dynamic')
     albums = db.relationship('ArtistToAlbum', backref='featured_artist', lazy='dynamic')
     songs = db.relationship('ArtistToSong', backref='song_creator', lazy='dynamic')
